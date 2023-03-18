738e70c1817632f1816f38908cf1aaf7d24e64e3
mihail-soltan <mihail.soltan@protonmail.ch>
2023-03-18 23:42:38 +0200

    add fancy font select + darkmode

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,20 +3,35 @@ import { SharedService } from 'src/app/services/shared.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  constructor(private sharedService: SharedService) {}
+  darkmode: boolean = false;
+  fonts: any[] = [
+    { font: 'Sans Serif', class: 'inter' },
+    { font: 'Serif', class: 'lora' },
+    { font: 'Mono', class: 'inconsolata' },
+  ];
+  currentFont: string = 'Sans Serif';
+  isFontSelectOpen: boolean = false;
 
-  constructor(private sharedService: SharedService) { }
-darkmode: boolean = false;
   ngOnInit(): void {
     this.sharedService.darkmode.subscribe((value: boolean) => {
       this.darkmode = value;
-    })
+    });
   }
 
   toggleTheme() {
     this.sharedService.toggleTheme();
-    console.log(this.darkmode)
+  }
+
+  toggleFontSelect() {
+    this.isFontSelectOpen = !this.isFontSelectOpen;
+  }
+
+  changeFont(font: string) {
+    this.currentFont = font;
+    this.toggleFontSelect();
   }
 }
